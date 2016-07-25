a90fed8a641207bf4dfa7869371f8ddb00396420
erlrange <erlrange@gmail.com>
2016-07-25 21:54:53 +0300

    Add apps to projects

--- eightwizards/eightwizards/settings/base.py
+++ eightwizards/eightwizards/settings/base.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'configuration',
+    'projects',
+    'team'
 ]
 
 DEBUG_APPS = [
@@ -109,11 +113,11 @@ USE_TZ = True
 
 # STATIC_ROOT = os.path.join(BASE, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../static'),
 ]
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
 MEDIA_URL = '/media/'
 
 # General project information
