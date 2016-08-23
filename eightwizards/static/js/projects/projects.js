0dfaf489b18123c03dcd75a5f235270592b949e6
Dmitry <dfkillovat@gmail.com>
2016-08-23 17:18:03 +0300

    adaptive project page

--- eightwizards/static/js/projects/projects.js
+++ eightwizards/static/js/projects/projects.js
@@ -19,4 +19,20 @@ angular.module('8wizards.projects', ['ngRoute', '8wizards.projects.services'])
   Projects.query(function(projects) {
     $scope.projects = projects;
   });
+   $scope.slickConfig = {
+    enabled: true,
+    autoplay: true,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    draggable: true,
+    autoplaySpeed: 3000,
+    method: {},
+    event: {
+        beforeChange: function (event, slick, currentSlide, nextSlide) {
+        },
+        afterChange: function (event, slick, currentSlide, nextSlide) {
+        }
+    }
+};
 }]);
