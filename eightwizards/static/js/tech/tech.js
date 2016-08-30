525f93b316a917e14c092f3567c6e66efdb34e19
NIck Sheglov <nektoneket@yandex.ua>
2016-08-30 17:19:07 +0300

    a

--- eightwizards/static/js/tech/tech.js
+++ eightwizards/static/js/tech/tech.js
@@ -26,6 +26,7 @@ angular.module('8wizards.tech', ['ngRoute', '8wizards.tech.services'])
 .controller('TechCtrl', ['$scope', '$controller', 'Technology', function($scope, $controller, Technology) {
   $controller('TechTabsCtrl', {$scope: $scope});
   $scope.selected = 0;
+
     $scope.breakpoints = [{
       breakpoint: 768,
       settings: {
