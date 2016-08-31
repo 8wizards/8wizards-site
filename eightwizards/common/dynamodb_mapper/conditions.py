3abd0d1906f7c706b9fa10f300d846bf2e3288bb
erlrange <erlrange@gmail.com>
2016-08-31 19:33:00 +0300

    Remove useless dynamodb mapper

--- eightwizards/common/dynamodb_mapper/conditions.py
+++
@@ -1,3 +0,0 @@
-# pylint: disable=all
-
-from boto3.dynamodb.conditions import *
--- eightwizards/common/dynamodb_mapper/types.py
+++
@@ -1,4 +0,0 @@
-# pylint: disable=unused-wildcard-import
-from boto3.dynamodb.types import *
-
-ALLOWED_DYNAMODB_TYPES = (NUMBER, STRING, STRING_SET, NUMBER_SET, NULL, BINARY, BINARY_SET, BOOLEAN, LIST, MAP)
