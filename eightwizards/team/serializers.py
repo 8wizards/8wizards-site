9c517440749e009f61a49c16f5c3ea552b95361b
NIck Sheglov <nektoneket@yandex.ua>
2016-09-01 15:22:53 +0300

    added RoleSerializer

--- eightwizards/team/serializers.py
+++ eightwizards/team/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
-from .models import Member, PromoUrl, Certification
+from .models import Member, PromoUrl, Certification, Role
+
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
 
 
 class MemberSerializer(serializers.ModelSerializer):
     """
     Controls serialization of Member model for REST API.
     """
+    role = RoleSerializer(read_only=True)
+
     class Meta:
         model = Member
+
 
 
 class PromoUrlSerializer(serializers.ModelSerializer):
