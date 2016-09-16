8ae0e992a22ae2448658ea9bf96a2abd5cace494
erlrange <erlrange@gmail.com>
2016-09-17 02:02:38 +0300

    Json Renderer

--- eightwizards/eightwizards/settings/base.py
+++ eightwizards/eightwizards/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&a59@nx#f7ms1=0dk=)8yn_kxyzy)io(kac-(qfavm4vg5g17e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get('DEBUG', False))
+DEBUG = int(os.environ.get('DEBUG', True))
 
 ALLOWED_HOSTS = []
 
@@ -172,4 +172,6 @@ LOGGING = {
 REST_FRAMEWORK = {}
 
 if not DEBUG:
-    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = ('rest_framework.renderers.JSONRenderer',)
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
+        'rest_framework.renderers.JSONRenderer',
+    )
